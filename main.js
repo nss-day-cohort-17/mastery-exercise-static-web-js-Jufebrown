82e2bcb47e2902830351965e0cdfe10d16e44e3f
Jufebrown <jufebrown@gmail.com>
2016-12-01 15:49:54 -0600

    moved comment

--- main.js
+++ main.js
@@ -2,13 +2,6 @@
 
 // It is absolutely critical that you read each of these requirements because as a professional you will be expected to completely understand what you are expected to do. If you submit code that does not fulfill the requirements, you will be sent to your room with no dinner.
 
-
-
-
-
-// Once the user enters in a number, and a character, the user can either then just press the enter key _(as long as the cursor is in one of the input fields)_, or click a button that is labeled "Grow your tree" and the tree should be shown in the console. This requires you to add an event listener to the button, as well as an event listener for the enter/return key.
-
-
 // ##### Example
 
 // Here's what the pine tree should look like when you specify a height of 7, and use the asterisk character.
@@ -53,10 +46,14 @@ function inputCheck () {
 
 // Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console.
 function tree (treeObj) {
+  for (var i = 0; i < treeObj.height; i++) {
+    console.log(treeObj.character)
+   }
 
-  console.log(???????????)
 }
 
+
+// Once the user enters in a number, and a character, the user can either then just press the enter key _(as long as the cursor is in one of the input fields)_, or click a button that is labeled "Grow your tree" and the tree should be shown in the console. This requires you to add an event listener to the button, as well as an event listener for the enter/return key.
 var buttonClick = document.getElementsByTagName("button").addEventListener('click', inputCheck);
 
 var enterHit = document.getElementsByTagName("input").addEventListener('keyup', enterCheck);
