345fcf400f039b14b3ba3c4c6d569610303d4dd9
Jufebrown <jufebrown@gmail.com>
2016-12-01 16:21:56 -0600

    moved comments

--- main.js
+++ main.js
@@ -19,19 +19,16 @@
 
 
 
+  // ### It accepts a single object as an argument. The object should have two key/value pairs.
+  var treeObj = {}
 
 
 // If either of the input fields does not have a value in it when the user presses the enter key, or presses the button, then display an alert stating that both fields must have a value.
 function inputCheck () {
-  // ### It accepts a single object as an argument. The object should have two key/value pairs.
-  var treeObj = {
   // 1. A key that specifies the height of the pine tree.
-    height: 0,
-  // 1. A key that specifies which character to use to build the pine tree.
-    character: "",
-  }
   // 1. The value for the height of the tree should be from user input in a `<input type="text">` field in the DOM.
   treeObj.height = parseInt(document.getElementsByName("height").value);
+  // 1. A key that specifies which character to use to build the pine tree.
   // 1. The character to use should be from user input in a `<input type="text">` field in the DOM.
   treeObj.character = document.getElementsByName("character").value;
   if (document.getElementsByName("height") === "") {
